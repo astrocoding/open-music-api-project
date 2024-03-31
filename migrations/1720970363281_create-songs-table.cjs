dfbe1aaaf77f0d6b4b4a2776b133ff64d621aea3
astrocoding <zaenalalfian20@gmail.com>
2024-03-31 11:27:26 +0700

    Add migration file
    Migration file for postgresql

---
+++ migrations/1720970363281_create-songs-table.cjs
@@ -0,0 +1,42 @@
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'varchar(30)',
+      primaryKey: true
+    },
+    title: {
+      type: 'varchar(50)',
+      notNull: true
+    },
+    year: {
+      type: 'integer',
+      notNull: true
+    },
+    performer: {
+      type: 'varchar(50)',
+      notNull: true
+    },
+    genre: {
+      type: 'varchar(50)',
+      notNull: true
+    },
+    duration: {
+      type: 'integer'
+    },
+    album_id: {
+      type: 'varchar(30)'
+    },
+    created_at: {
+      type: 'text',
+      notNull: true
+    },
+    updated_at: {
+      type: 'text',
+      notNull: true
+    }
+  })
+}
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs')
+}
